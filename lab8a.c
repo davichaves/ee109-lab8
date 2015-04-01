a6fa18cd59ae3b6fc5111695cf507aa5cf860cd0
Davi Chaves <dchaves@usc.edu>
2015-04-01 16:09:25 -0700

    part A complete

--- lab8a.c
+++ lab8a.c
@@ -2,7 +2,7 @@
 *
 *  Name: Davi Rodrigues Chaves
 *  Section: W 3:30-5:00 PM
-*  Assignment: Lab 8 - Rotary Encoders
+*  Assignment: Lab 8a - Rotary Encoders
 *
 ********************************************/
 
@@ -13,43 +13,72 @@
 #include "lcd.h"
 
 void init_ports(void);
-void check_states(void);
 
 int main(void) {
-  //sei(); // Enable interrupts
-  init_ports();
-	init_lcd();
-  writecommand(1); //clear LCD
-  char myVar[20];
-  int var = -10;
-  moveto(5);
-  int aState, bState;
-  /* Main programs goes here */
-  while (1) {
-    if ((PINC & 0b00010000) == 0) { //A5 == 0
-      aState = 0;
-    } else {
-      aState = 1;
-    }
-    if ((PINC & 0b00100000) == 0) { //A4 == 0
-      bState = 0;
-    } else {
-      bState = 1;
-    }
+    //sei(); // Enable interrupts
+    init_ports();
+    init_lcd();
+    writecommand(1); //clear LCD
+    char myVar[20];
+    int var = 0;
+    int aState, bState, prevAState, prevBState;
+    /* Main programs goes here */
+    while (1) {
 
-    if (/* condition */) {
-      /* code */
-    }
-    snprintf(myVar, 20, "%d:%d", aState, bState);
-    stringout(myVar);
-    moveto(5);
-  } // Loop forever
-
-  return 0;   /* never reached */
-}
-
-void check_states() {
-
+        prevAState = aState;
+        prevBState = bState;
+        if ((PINC & 0b00010000) == 0) { //A5 == 0
+            aState = 0;
+        } else {
+            aState = 1;
+        }
+        if ((PINC & 0b00100000) == 0) { //A4 == 0
+            bState = 0;
+        } else {
+            bState = 1;
+        }
+        if (aState != prevAState || bState != prevBState) { // if it changes do something
+            if (aState == 0 && bState == 0) {
+                if (prevAState == 1) {
+                    var+=1;
+                }
+                if (prevBState == 1) {
+                    var-=1;
+                }
+            }
+            if (aState == 0 && bState == 1) {
+                if (prevAState == 1) {
+                    var-=1;
+                }
+                if (prevBState == 0) {
+                    var+=1;
+                }
+            }
+            if (aState == 1 && bState == 1) {
+                if (prevAState == 0) {
+                    var+=1;
+                }
+                if (prevBState == 0) {
+                    var-=1;
+                }
+            }
+            if (aState == 1 && bState == 0) {
+                if (prevAState == 0) {
+                    var-=1;
+                }
+                if (prevBState == 1) {
+                    var+=1;
+                }
+            }
+            snprintf(myVar, 20, "%d", var);
+            stringout("    ");
+            moveto(0);
+            stringout(myVar);
+            moveto(0);
+        }
+        
+    } // Loop forever
+    return 0;   /* never reached */
 }
 
 /*
@@ -57,8 +86,8 @@ void check_states() {
 */
 
 void init_ports() {
-  PCMSK0 |= ((1 << PCINT5)| (1 << PCINT1));
-  DDRD |= 0xF0; //setting D4-D7 as outputs
-  DDRB |= 0x03; //seeting B1-B2 as outputs
-  PORTC |= ((1 << PC4) | (1 << PC5));
+    PCMSK0 |= ((1 << PCINT5)| (1 << PCINT1));
+    DDRD |= 0xF0; //setting D4-D7 as outputs
+    DDRB |= 0x03; //seeting B1-B2 as outputs
+    PORTC |= ((1 << PC4) | (1 << PC5));
 }
